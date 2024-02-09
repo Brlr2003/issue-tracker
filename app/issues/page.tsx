b9add2d0130eb6e145806aea04e249081bff3e50
Brlr2003 <omaralsafarti03@gmail.com>
2024-02-09 23:56:58 +0100

    Refractor: Organize import statements

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,10 +1,7 @@
-import React from "react";
-import Link from "../components/Link";
-import prisma from "@/prisma/client";
-import IssueStatusBadge from "../components/IssueStatusBadge";
-import delay from "delay";
-import IssueActions from "./IssueActions";
 import { Table } from "@radix-ui/themes";
+import { IssueStatusBadge, Link } from "@/app/components";
+import IssueActions from "./IssueActions";
+import prisma from "@/prisma/client";
 
 const IssuesPage = async () => {
   const issues = await prisma.issue.findMany();
